e4c75ea7ad00ded647bfa40111e553dfdcaa42ee
crian <crian84@gmail.com>
2019-11-17 23:15:43 +0100

    config: Rename compton to picom

--- .config/dwm/autostart.sh
+++ .config/dwm/autostart.sh
@@ -2,7 +2,7 @@
 
 ~/.config/dwm/bar &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-compton --config ~/.config/compton.conf &
+picom --config ~/.config/picom.conf &
 nitrogen --restore &
 dunst &
 clipmenud &
