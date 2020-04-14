1ed03a816dd5b685886981afa1c221ae4881bc86
crian <crian84@gmail.com>
2020-04-14 22:53:44 +0200

    suckless: dwmblocks: blocks: Remove icons

--- .suckless/dwmblocks/blocks.h
+++ .suckless/dwmblocks/blocks.h
@@ -1,19 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"", "~/.config/dwm/blocks/spotify", 1, 0},
+    /*Command*/    /*Update Interval*/    /*Update Signal*/
+    {"~/.config/dwm/blocks/spotify", 1, 0},
 
-    {"", "~/.config/dwm/blocks/net", 10, 0},
+    {"~/.config/dwm/blocks/net", 10, 0},
 
-    {"", "~/.config/dwm/blocks/battery", 5, 0},
+    {"~/.config/dwm/blocks/battery", 5, 0},
 
-    {"", "~/.config/dwm/blocks/backlight", 0, 11},
+    {"~/.config/dwm/blocks/backlight", 0, 11},
 
-    {"", "~/.config/dwm/blocks/volume", 0, 10},
+    {"~/.config/dwm/blocks/volume", 0, 10},
 
-    {"", "~/.config/dwm/blocks/date", 60, 0},
+    {"~/.config/dwm/blocks/date", 60, 0},
 
-    {"", "~/.config/dwm/blocks/time", 60, 0},
+    {"~/.config/dwm/blocks/time", 60, 0},
 };
 
 //sets delimeter between status commands. NULL pointer means no delimeter.
